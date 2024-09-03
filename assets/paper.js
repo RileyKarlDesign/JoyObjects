10fbaf92cfe2873d85117c2e3b3b964da2df65d6
Riley Karl <rileykarldesign@gmail.com>
2024-09-03 23:19:32 +0100

    add

--- assets/paper.js
+++ assets/paper.js
@@ -1,18 +1,3 @@
 
 
 
-window.onload = function () {
- 
-console.log('paper')
-
-  // Get a reference to the canvas object
-  var canvas = document.getElementById("myCanvas");
-   paper.setup(document.getElementById('mainCanvas'));
-  var c = paper.Shape.Circle(200, 200, 50);
-  c.fillColor = 'green';
-
-  paper.view.draw();
-
-
-
-};
